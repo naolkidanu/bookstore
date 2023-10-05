e6945d83c937608875d7ca284ef9a78449e8db4c
Naol Kidanu <sabelkidanu18@gmail.com>
2023-10-05 12:51:57 +0300

    Apply a reducer that checks the status

--- src/components/redux/catagories/catagoriesSlice.js
+++ src/components/redux/catagories/catagoriesSlice.js
@@ -3,3 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   categories: [],
 };
+
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {
+    addCategory: (state, action) => {
+      state.categories.push(action.payload);
+    },
+  },
+});
+
+export const selectStatus = (state) => "Under construction";
+
+export const { addCategory } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;

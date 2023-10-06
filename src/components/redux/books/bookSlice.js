78b6e8f3fceb9cadcdce8bdacf2f2d3172497a50
Naol Kidanu <sabelkidanu18@gmail.com>
2023-10-06 13:09:14 +0300

    Use of bookstore api

--- src/components/redux/books/bookSlice.js
+++ src/components/redux/books/bookSlice.js
@@ -1,30 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+
+const appId = "JyK4gWmXitzJmJBDoo80";
+const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}/books`;
 
 const initialState = {
   books: [
     {
-      id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
+      id: "item1",
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
     },
     {
-      id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
+      id: "item2",
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
     },
     {
-      id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
+      id: "item3",
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
     },
   ],
 };
 
 export const bookSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: (state, action) => {
